744ce9d11108d12add8ed6ac86c0f41a49263352
Ahmed Genedy <agenedy75@gamil.com>
2025-11-30 03:26:59 +0200

    Edit Nav & Creat footer

--- js/loadNavbar.js
+++ js/loadNavbar.js
@@ -1,10 +1,8 @@
-// Load the navbar into all pages
 fetch("navbar.html")
   .then(res => res.text())
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
 
-    // After loading navbar, enable scroll shrink effect
     const header = document.querySelector("header");
 
     window.addEventListener("scroll", function () {
